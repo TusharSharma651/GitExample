195294b1ba4a95901a4bec3e827ab5676bf069b4
Tushar Sharma <tusarjn@gmail.com>
2018-05-06 21:06:41 +0530

    Added new Map Feature

---
+++ GitExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitExample
+//
+//  Created by Tushar on 06/05/18.
+//  Copyright © 2018 Tushar. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+
+}
